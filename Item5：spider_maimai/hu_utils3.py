4b87bbed9f84cf7272c5d8e140f5529735eca619
胡权祥 <hqx@lezhi.com>
2018-12-13 09:52:05 +0800

    初次提交

---
+++ Item5：spider_maimai/hu_utils3.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:吉祥鸟
+# datetime:2018/10/22 14:41
+# software: PyCharm
+import time
+import pymysql
+import requests
+import random
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def open_local_db(db="spider"):
+    """
+    开启本地数据库
+    :param db: 数据库（默认为spider）
+    :return: 创建好的数据库连接
+    """
+    print(now_time(), '连接本地数据库%s' % db)
+    conn = pymysql.connect(host='192.168.1.4', user='root', passwd='666', db=db, port=3306, charset='gbk')
+    return conn
+
+
+def open_line_db(db="lz_datastore"):
+    """
+    开启线上数据库
+    :param db: 数据库（默认为spider）
+    :return: 创建好的数据库连接
+    """
+    print(now_time(), '连接线上数据库%s' % db)
+    conn = pymysql.connect(host='rm-2zeuz492sf50a2t27ko.mysql.rds.aliyuncs.com',
+                           user='gcdata2012',
+                           passwd='gdlz_2017',
+                           db=db,
+                           port=3306,
+                           charset='gbk')
+    return conn
+
+
+def SpCharReplace(char):
+    # char=char
+    temp = str(char)
+    for i in temp:
+        if '<' == i:
+            char = char.replace('<', '《')
+        if '>' == i:
+            char = char.replace('>', '》')
+        if '\'' == i:
+            char = char.replace('\'', '')  # 处理单引号
+        if '\\' == i:
+            char = char.replace('\\', '')  # 处理反斜杠\
+        if '\"' == i:
+            char = char.replace('\"', '`')  # 处理双引号"
+        if '&' == i:
+            char = char.replace('&', '-')  # 处理&号"
+        if '|' == i:
+            char = char.replace('|', '')  # 处理&号
+        if '@' == i:
+            char = char.replace('@', '.')  # 处理@号
+        if '%' == i:
+            char = char.replace('%', "`")  # 处理单引号
+        if '*' == i:
+            char = char.replace('*', '`')  # 处理反斜杠\
+        if '("' == i:
+            char = char.replace('\"', '`')  # 处理双引号"
+        if ')"' == i:
+            char = char.replace(')"', '`')
+        if '-' == i:
+            char = char.replace('-', '`')  # 处理&号
+        if 'ÐÂÎÅ' == i:
+            char = char.replace('ÐÂÎÅ', '`')  # 处理ÐÂÎÅ
+        # 在后面扩展其他特殊字符
+    return char
+
+
+def select_one(conn):
+    """
+    查询信息
+    :param time:
+    :param type:
+    :return:
+    """
+    cursor = conn.cursor()
+    sql = "select ios_id from ios_main"
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        conn.commit()
+        return result
+    except:
+        print(now_time(), '更新数据失败，回滚')
+        conn.rollback()
+    conn.close()
+
+
+def insert_update_one(conn, item, table_name,status=True):
+    """
+    单条数据插入或更新到数据库（注：插入的数据包含表里的关键字）
+    :param conn:数据库
+    :param item:插入的数据字典（字典类型）
+    :param table_name:数据库表名
+    :return:无
+    """
+    print(now_time(), "更新数据表{}.....".format(table_name))
+    # print("item:", item)
+    if item:
+        cursor = conn.cursor()
+        sql1 = "insert into %s" % table_name
+        sql2 = "("
+        sql3 = ") values("
+        sql4 = ")on duplicate key update "
+        for key in item.keys():  # 拼接sql语句
+            sql2 += "%s," % key
+            sql3 += "%s,"
+            sql4 += "%s=values(%s)," % (key, key)
+        # try:
+        item_values = list(item.values())
+        item_values[1] = str(item_values[1])
+        # print item_values
+        sql = sql1 + sql2[:-1] + sql3[:-1] + sql4[:-1]
+        # print(sql)
+        cursor.execute(sql, item_values)
+        conn.commit()
+        print(now_time(), "数据更新成功")
+        # except:
+        #     print(now_time(), '更新数据失败，回滚')
+        #     conn.rollback()
+    else:
+        print("无数据")
+    if status:
+        conn.close()
+
+
+def insert_update_many(conn, items, table_name,status=True):
+    """
+    多条数据插入或更新到数据库（注：插入的数据包含表里的关键字）
+    :param conn:数据库
+    :param items:插入的数据列表字典（列表内包含字典类型）
+    :param table_name:数据库表名
+    :return:无
+    """
+    print(now_time(), "更新数据表{}...".format(table_name))
+    # print("item:", items)
+    if items:
+        cursor = conn.cursor()
+        sql1 = "insert into %s" % table_name
+        sql2 = "("
+        sql3 = ") values("
+        sql4 = ")on duplicate key update "
+        for key in items[0].keys():  # 拼接sql语句
+            sql2 += "%s," % key
+            sql3 += "%s,"
+            sql4 += "%s=values(%s)," % (key, key)
+        sql = sql1 + sql2[:-1] + sql3[:-1] + sql4[:-1]
+        item_values = []
+        for item in items:
+            item_values.append(list(item.values()))
+        num = len(item_values)
+        print(now_time(), '一共需要处理数据%s条' % num)
+
+        # try:
+        for i in range(0, num, 1000):
+            a = min(num, 1000 + i)
+            cursor.executemany(sql, item_values[i:a])
+            conn.commit()
+            print(now_time(), "当前已经处理%s条数据" % a)
+        # except:
+        #     print(now_time(), '更新数据失败，回滚')
+        #     conn.rollback()
+    if status:
+        conn.close()
+
+
+def now_time():
+    """
+    格式化返回当前时间
+    :return:
+    """
+    now = int(time.time())
+    local_time = time.localtime(now)
+    format_now = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+    return format_now
---
+++ Item5：spider_maimai/login.py
@@ -0,0 +1,326 @@
+# -*- coding: UTF-8 -*-
+# author:吉祥鸟
+# datetime:2018/12/6 13:48
+# software: PyCharm
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+import requests
+import json
+import re
+import random
+import hu_utils3
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+# 定义一个RotatingFileHandler，最多备份3个日志文件，每个日志文件最大1K
+rHandler = RotatingFileHandler("log.txt", maxBytes=10 * 1024, backupCount=3)
+rHandler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+rHandler.setFormatter(formatter)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logger.addHandler(rHandler)
+logger.addHandler(console)
+
+
+def random_steep():
+    """
+    防止封号，随机暂停
+    :return:
+    """
+    a = random.randint(2, 5)
+    logger.info("暂停{}秒".format(a))
+    time.sleep(a)
+
+
+def get_cookies():
+    """
+    使用selenium获取cookies的值，将其存在文件中
+    :return:
+    """
+    logger.info("使用selenium获取cookies")
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+    chrome_options.add_argument('--disable-gpu')  # 谷歌文件提到需要加这个属性来规避bug
+    chrome_options.add_argument('--headless')  # 无界面设置
+    chrome = webdriver.Chrome(chrome_options=chrome_options)
+    login_url = "https://acc.maimai.cn/login"
+    chrome.get(login_url)
+    wait = WebDriverWait(chrome, 10)
+    wait.until(EC.element_to_be_clickable(('xpath', "//input[@class='loginBtn']")))
+    time.sleep(1)
+    user_name = "15755656557"
+    password = "199618hu"
+    chrome.find_element("xpath", "//input[@class='loginPhoneInput']").send_keys(user_name)
+    time.sleep(1)
+    chrome.find_element("xpath", "//input[@id='login_pw']").send_keys(password)
+    chrome.find_element('xpath', "//input[@class='loginBtn']").click()
+    cookies = chrome.get_cookies()
+    with open("cookie.json", "w+")as f:
+        f.write(json.dumps(cookies))
+        f.close()
+    logger.info("cookies获取成功")
+    chrome.close()
+
+
+def get_req():
+    """
+    打开cookies文件获取cookies，进行requests请求
+    :return:
+    """
+    logger.info("打开cookies文件获取cookies")
+    with open("cookie.json", "r")as f:
+        cookies = json.loads(f.readline())
+    req = requests.Session()
+    for cookie in cookies:
+        req.cookies.set(cookie["name"], cookie["value"])
+    return req
+
+
+def cookies_expried():
+    """
+    判断cookies是否过期，若过期会自动登录获取cookies
+    :return:
+    """
+    req = get_req()
+    url = "https://maimai.cn/web/search_center?type=contact&query=cho&highlight=true"
+    response = req.get(url)
+    logger.info("验证cookies是否可用")
+    login_state = True
+    if "登录到脉脉" in response.text:
+        login_state = False
+    if login_state:
+        logger.info("cookies可用")
+        return req
+    else:
+        logger.info("cookies不可用，重新获取cookies")
+        get_cookies()
+        req = get_req()
+        return req
+
+
+def json_info(html):
+    """
+    处理获取的html，转换为json格式
+    :param html:
+    :return:
+    """
+    c = re.search('JSON\.parse\("(.*?)"\);</script><script\ssrc=', html.text, re.S).group(1)  # 正则匹配所需要的数据
+    d = c.replace('\\u0022', '"').replace("\\u002D", "-").replace("\\u0026", '&').replace("\\u005C", "\\")  # 对数据进行处理
+    data = json.loads(d)
+    return data
+
+
+def basic_info(data):
+    """
+    基本信息表--表1
+    :param data:
+    :return:
+    """
+    basic_info = {}
+    basic_info['name'] = data["card"]["name"]  # 名字
+    basic_info['mmid'] = int(data["card"]["mmid"])  # 脉脉id
+    basic_info['rank'] = data["card"]["rank"]  # 影响力
+    basic_info['company'] = data["card"]["company"]  # 目前公司简称
+    basic_info['stdname'] = data["company"]["stdname"]  # 目前公司全称
+    basic_info['position'] = data["card"]["position"]  # 目前职位
+    basic_info['headline'] = data["uinfo"]["headline"]  # 自我介绍
+    if basic_info["headline"]:
+        basic_info['headline'] = basic_info['headline'].encode('gbk', errors='ignore').decode('gbk')
+    try:
+        basic_info['ht_province'] = data["uinfo"]["ht_province"]  # 家乡-省
+    except:
+        logger.info("家乡-省获取信息不存在")
+        basic_info['ht_province'] = ""
+    try:
+        basic_info['ht_city'] = data["uinfo"]["ht_city"]  # 家乡-城市
+    except:
+        logger.info("家乡-城市获取信息不存在")
+        basic_info['ht_city'] = ""
+    basic_info['email'] = data["uinfo"]["email"]
+    basic_info['mobile'] = data["uinfo"]["mobile"]  # 手机号
+    basic_info['dist'] = data["card"]["dist"]  # 几度关系
+    # logger.info('基本信息表:', basic_info)
+    return basic_info
+
+
+def work_exp(id_num, req, data):
+    """
+    工作经历--表2
+    :param req:
+    :param data:
+    :param start_url:
+    :return:
+    """
+    work_exps = []
+    work_expzz = data["uinfo"]["work_exp"]
+    for work_expz in work_expzz:
+        work_exp = {}
+        work_exp["start_year"] = 0000
+        work_exp["start_mon"] = 0000
+        work_exp["end_year"] = 0000
+        work_exp["end_mon"] = 0000
+        share_url = work_expz["company_info"]["share_url"]
+        #  logger.info('start_url', share_url)
+        random_steep()
+        start_html = req.get(share_url)  # 用于获取的是公司的全称
+        dataz = json_info(start_html)
+        fullname = dataz["data"]["data"]["cinfo"]["fullname"]
+        start_date = work_expz["start_date"]
+        end_date = work_expz["end_date"]
+        work_exp["company"] = work_expz["company"]
+        work_exp["stdname"] = fullname
+        work_exp["et_url"] = share_url
+        if "-" in str(start_date):
+            work_exp["start_year"] = int(start_date.split("-")[0])
+            work_exp["start_mon"] = int(start_date.split("-")[1])
+        if "-" in str(end_date):
+            work_exp["end_year"] = int(end_date.split("-")[0])
+            work_exp["end_mon"] = int(end_date.split("-")[1])
+        work_exp["position"] = work_expz["position"]
+        work_exp["mmid"] = id_num
+        work_exp["description"] = work_expz["description"]
+        if work_exp["description"]:
+            work_exp["description"] = work_exp["description"].encode('gbk', errors='ignore').decode('gbk')
+        # logger.info("工作经历：", work_exp)
+        work_exps.append(work_exp)
+    return work_exps
+
+
+def education_exp(mmid, data):
+    """
+    教育经历--表3
+    :param data:
+    :return:
+    """
+    education_exps = []
+    educations = data["uinfo"]["education"]
+    for education in educations:
+        education_exp = {}
+        education_exp['school_name'] = education["school"]
+        education_exp['department'] = education["department"]
+        try:
+            education_exp["start_year"] = int(education["start"])
+        except:
+            education_exp["start_year"] = 0000
+        try:
+            education_exp["start_mon"] = int(education["start_month"])
+        except:
+            education_exp["start_mon"] = 0000
+        try:
+            education_exp["end_year"] = int(education["end"])
+        except:
+            education_exp["end_year"] = 0000
+        try:
+            education_exp["end_mon"] = int(education["end_month"])
+        except:
+            education_exp["end_mon"] = 0000
+        education_exp["mmid"] = mmid
+
+        education_exp["education"] = education["degree"]
+        # logger.info('教育经历', education_exp)
+        education_exps.append(education_exp)
+    return education_exps
+
+
+def tag_info(mmid, data):
+    # 标签信息 --表4
+    tag_infos = []
+    tags = data["uinfo"]["weibo_tags"]
+    tag_weights = data["uinfo"]["tag_weights"]
+    for tag in tags:
+        tag_info = {}
+        tag_info["tag"] = tag
+        if tag in tag_weights.keys():
+            tag_info["rec_num"] = int(tag_weights[str(tag)]["weight"])
+        else:
+            tag_info["rec_num"] = 1
+        tag_info["mmid"] = mmid
+        tag_infos.append(tag_info)
+        # logger.info("标签信息:", tag_info)
+    return tag_infos
+
+
+def review_info(mmid, data):
+    """
+    点评信息 --表5
+    :param mmid:
+    :param data:
+    :return:
+    """
+    data = json.loads(data)
+    review_infos = []
+    evaluation_list = data["data"]["evaluation_list"]
+    for evaluation in evaluation_list:
+        review_info = {}
+        review_info['reviewer'] = evaluation["src_user"]['name']
+        review_info['relationship'] = evaluation["re"]
+        review_info['position'] = evaluation["src_user"]["career"]
+        review_info['eva_info'] = evaluation["text"].encode('gbk', errors='ignore').decode('gbk')
+        review_info['mmid'] = mmid
+        # logger.info('点评信息:', review_info)
+        review_infos.append(review_info)
+    # logger.info('review_infos:', review_infos)
+    return review_infos
+
+
+def main():
+    cai_num = 1
+    req = cookies_expried()
+    a = 'https://maimai.cn/search/contacts?count=20&page='
+    b = '&query=cho&dist=0&searchTokens=["cho"]&highlight=true&jsononly=1'
+    for i in range(30):  # 列表页
+        start_url = a + str(i) + b
+        random_steep()
+        response = req.get(url=start_url)
+        res_json = response.json()  # 如果为空，则无数据
+        infos = res_json["data"]["contacts"]  # 信息列表
+        logger.info("本页信息条数：{}".format(len(infos)))
+        if len(infos) == 0:
+            break
+        for info in infos:  # 详情页
+            logger.info("正在获取第{}条数据".format(cai_num))
+            cai_num += 1
+            encode_mmid = info["contact"]["encode_mmid"]
+            two_url = "https://maimai.cn/contact/detail/" + encode_mmid
+            review_info_url = "https://maimai.cn/contact/comment_list/" + encode_mmid + "?jsononly=1"
+            random_steep()
+            html = req.get(two_url)  # 详细页面的信息
+            basic_data = json_info(html)
+            data = basic_data["data"]
+            # plogger.info(data)
+            id_num = int(data["card"]["mmid"])  # mmid
+            logger.info("mmid:{}".format(id_num))
+            time.sleep(1)
+            # 基本信息--表1
+            basic_infos = basic_info(data)
+            conn = hu_utils3.open_local_db("caiji_maimai")
+            try:
+                hu_utils3.insert_update_one(conn, basic_infos, "basic_info", False)
+                # 工作经历--表2
+                work_exps = work_exp(id_num, req, data)
+                hu_utils3.insert_update_many(conn, work_exps, 'work_exp', False)
+                # 教育经历--表3
+                education_exps = education_exp(id_num, data)
+                hu_utils3.insert_update_many(conn, education_exps, 'education_exp', False)
+                # 标签信息--表4
+                tag_infos = tag_info(id_num, data)
+                hu_utils3.insert_update_many(conn, tag_infos, 'tag_info', False)
+                # 点评信息 --表5
+                random_steep()
+                review_data = req.get(review_info_url).text  # 点评页面
+                review_infos = review_info(id_num, review_data)
+                hu_utils3.insert_update_many(conn, review_infos, 'review_info', False)
+            finally:
+                logger.info("关闭数据库")
+                conn.close()
+
+
+if __name__ == "__main__":
+    main()
